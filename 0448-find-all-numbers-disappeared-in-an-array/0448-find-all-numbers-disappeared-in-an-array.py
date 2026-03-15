783d89d797b1dda45538f6c58637429425f987d0
ksahithir <ksahithir3011@gmail.com>
2026-03-15 11:55:24 +0530

    Time: 29 ms (85.2%), Space: 26.7 MB (98.57%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        num_set =set(nums)
+        array=[]
+        for i in range(1,n+1):
+            if i not in num_set:
+                array.append(i)
+        return array
+        
