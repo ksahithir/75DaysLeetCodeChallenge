df9687472383e3b812dca085b105415743030052
ksahithir <ksahithir3011@gmail.com>
2026-03-20 11:40:30 +0530

    Time: 0 ms (100%), Space: 12.3 MB (97.51%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        result=pow(x,n)
+        return result
+
+        
+        
+            
